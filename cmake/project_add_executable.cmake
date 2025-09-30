363f6a594865855db4e76c43827521410be6588c
Aaron Shelley <amshelley86@gmail.com>
2025-09-30 19:20:56 -0400

    Enable ctest unit testing and add to github workflows.

--- cmake/project_add_executable.cmake
+++ cmake/project_add_executable.cmake
@@ -9,13 +9,18 @@ function(project_add_executable target)
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     )
 
+    # If a `test` subdirectory exists in the component's source tree, add it.
+    # Use an absolute path to the source and a matching binary-dir so this works
+    # regardless of where this function was defined or executed.
     if(BUILD_TESTING AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/test)
-        add_subdirectory(test)
+        add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/test ${CMAKE_CURRENT_BINARY_DIR}/test)
     endif()
 
     # Make it possible to easily debug applications through the visual studio debugger.
+    # Make it possible to easily debug applications through the Visual Studio debugger.
     if(MSVC)
         set(PROJECT_VCPKG_ROOT ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET})
-        set_target_properties(${PROJECT_NAME} PROPERTIES VS_DEBUGGER_ENVIRONMENT "PATH=%PATH%;${PROJECT_VCPKG_ROOT}/bin")
+        # Use the actual target name passed into the function, not ${PROJECT_NAME}.
+        set_target_properties(${target} PROPERTIES VS_DEBUGGER_ENVIRONMENT "PATH=%PATH%;${PROJECT_VCPKG_ROOT}/bin")
     endif()
 endfunction()
--- cmake/project_add_library.cmake
+++ cmake/project_add_library.cmake
@@ -3,7 +3,9 @@ function(project_add_library)
 
     project_compile_lib()
 
+    # If the library has a `test` subdirectory, add it with an explicit
+    # binary-dir so test CMake files are generated in the library build tree.
     if(BUILD_TESTING AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/test)
-        add_subdirectory(test)
+        add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/test ${CMAKE_CURRENT_BINARY_DIR}/test)
     endif()
 endfunction()
