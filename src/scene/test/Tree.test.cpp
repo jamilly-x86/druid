d55e0fea8a6c3b78055e1acf4da409f91463c1b2
Aaron Shelley <amshelley86@gmail.com>
2025-11-11 21:42:37 -0500

    Implement dirty transform updates.

--- src/scene/test/Tree.test.cpp
+++ src/scene/test/Tree.test.cpp
@@ -1,19 +1,25 @@
 #include <druid/scene/Tree.h>
 #include <gtest/gtest.h>
 
+#include <glm/gtc/matrix_transform.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/matrix_decompose.hpp>
+
 TEST(Tree, CreateNode)
 {
-	 druid::scene::Tree tree;
-	 auto node = tree.create_node();
-	 EXPECT_NE(node, nullptr);
+	druid::scene::Tree tree;
+	auto node = tree.create_node();
+	EXPECT_NE(node, nullptr);
 }
 
-TEST(Tree, MakeDirty)
+TEST(Tree, SetTransform)
 {
-	 druid::scene::Tree tree;
-	 auto node = tree.create_node();
-	 node->make_dirty(druid::scene::Node::Dirty::Transform);
-	 // Since dirty_ is private, we cannot access it directly.
-	 // This test ensures that the method can be called without errors.
-	 SUCCEED();
+	druid::scene::Tree tree;
+	auto node = tree.create_node();
+	druid::scene::Transform transform;
+	transform.local = glm::translate(glm::mat4{1.0F}, glm::vec3(1.0F, 2.0F, 3.0F));
+	node->set_transform(transform);
+	const auto& retrieved_transform = node->get_transform();
+	EXPECT_EQ(retrieved_transform.local, transform.local);
 }
