f16aaf1f4f7e9e3338c12025a4a9d98669a7183f
Aaron Shelley <amshelley86@gmail.com>
2025-11-13 19:48:39 -0500

    Update node API to guarantee non-null addition and removal.

--- src/scene/test/Tree.test.cpp
+++ src/scene/test/Tree.test.cpp
@@ -39,7 +39,7 @@ TEST(Tree, NestedTransform)
 
 	parent_node->set_transform({.local = glm::translate(glm::mat4{1.0F}, pos1)});
 	child_node->set_transform({.local = glm::translate(glm::mat4{1.0F}, pos2)});
-	parent_node->add_child(child_node.get());
+	parent_node->add_child(*child_node);
 	tree.update_transforms();
 	const auto& parent_transform = parent_node->get_transform();
 	const auto& child_transform = child_node->get_transform();
@@ -52,4 +52,27 @@ TEST(Tree, NestedTransform)
 	EXPECT_EQ(translation, pos1);
 	glm::decompose(child_transform.global, scale, rotation, translation, skew, perspective);
 	EXPECT_EQ(translation, pos1 + pos2);
+}
+
+TEST(Tree, RemoveChild)
+{
+	druid::scene::Tree tree;
+	auto parent_node = tree.create_node();
+	auto child_node = tree.create_node();
+	constexpr auto pos1 = glm::vec3(2.0F, 0.0F, 0.0F);
+	constexpr auto pos2 = glm::vec3(0.0F, 2.0F, 0.0F);
+	parent_node->set_transform({.local = glm::translate(glm::mat4{1.0F}, pos1)});
+	child_node->set_transform({.local = glm::translate(glm::mat4{1.0F}, pos2)});
+	parent_node->add_child(*child_node);
+	tree.update_transforms();
+	parent_node->remove_child(*child_node);
+	tree.update_transforms();
+	const auto& child_transform = child_node->get_transform();
+	glm::vec3 scale;
+	glm::quat rotation;
+	glm::vec3 translation;
+	glm::vec3 skew;
+	glm::vec4 perspective;
+	glm::decompose(child_transform.global, scale, rotation, translation, skew, perspective);
+	EXPECT_EQ(translation, pos2);
 }
