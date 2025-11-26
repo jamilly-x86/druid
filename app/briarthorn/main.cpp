791dea3da6c384ee786d02495b8274610ff77bbf
Aaron Shelley <amshelley86@gmail.com>
2025-11-26 16:45:45 -0500

    Update enummask to use modules.

--- app/briarthorn/main.cpp
+++ app/briarthorn/main.cpp
@@ -1,16 +1,4 @@
-
-import druid.core.enummask;
-
-enum class Type : int
-{
-    One,
-    Two,
-    Three
-};
-
 auto main() -> int
 {
-    druid::core::EnumMask<Type> test{};
-    test.set(Type::One);
     return 0;
 }
