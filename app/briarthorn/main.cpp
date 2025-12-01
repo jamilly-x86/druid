29a624ad90863a96b36f691253e868487c1160cb
Aaron Shelley <amshelley86@gmail.com>
2025-11-30 19:38:43 -0500

    Working component rendering.

--- app/briarthorn/main.cpp
+++ app/briarthorn/main.cpp
@@ -10,26 +10,28 @@ auto main() -> int
 	flecs::world world;
 
 	// clang-format off
-	world.import<druid::raylib::Module>();
+	auto module = world.import<druid::raylib::Module>();
 	// clang-format on
 
 	auto window = world.entity();
 	window.set<druid::raylib::Window>({.title = "test", .width = 1280, .height = 720});
 
-	for (auto i = 0; i < 1000; i++)
+	for (auto i = 0; i < 50; i++)
 	{
-		auto e = world.entity().add<druid::raylib::Rectangle>();
-		auto &r = e.get_mut<druid::raylib::Rectangle>();
+		const auto angle = GetRandomValue(0, 360);
+		const auto radius = GetRandomValue(0, 360);
 
-		auto angle = GetRandomValue(0, 360);
-		auto radius = GetRandomValue(0, 360);
-
-		r.x = 640 + radius * std::cos(angle * DEG2RAD);
-		r.y = 360 + radius * std::sin(angle * DEG2RAD);
+		druid::raylib::Rectangle r{};
+		r.x = static_cast<int>(640 + radius * std::cos(angle * DEG2RAD));
+		r.y = static_cast<int>(360 + radius * std::sin(angle * DEG2RAD));
 		r.width = 25;
 		r.height = 25;
-		r.color = RED;
+		r.color = {125, 125, 125, 255};
+		world.entity().set<druid::raylib::Drawable>(r);
 	}
+
+	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::make_arc(640, 360, 100, -45, 300, 4, 120, { 255, 255, 0, 255 }));
+	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::make_arc(640, 360, 250, -50, 305, 4, 120, {255, 255, 0, 255}));
 
 	// Run indefinitely.
 	while (world.progress())
