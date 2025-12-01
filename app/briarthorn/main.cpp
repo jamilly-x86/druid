843c3c9d670ad97c1f9007c67d6ef46b1ee42575
Aaron Shelley <amshelley86@gmail.com>
2025-11-30 22:28:09 -0500

    Cleanup implementation of arc to trianglestrip conversion.

--- app/briarthorn/main.cpp
+++ app/briarthorn/main.cpp
@@ -8,6 +8,7 @@
 
 auto main() -> int
 {
+	// NOLINTBEGIN
 	flecs::world world;
 
 	// clang-format off
@@ -23,21 +24,26 @@ auto main() -> int
 		const auto radius = GetRandomValue(0, 360);
 
 		druid::raylib::Rectangle r{};
-		r.x = static_cast<int>(640 + radius * std::cos(angle * DEG2RAD));
-		r.y = static_cast<int>(360 + radius * std::sin(angle * DEG2RAD));
+		r.x = static_cast<int>(640 + (radius * std::cos(angle * DEG2RAD)));
+		r.y = static_cast<int>(360 + (radius * std::sin(angle * DEG2RAD)));
 		r.width = 25;
 		r.height = 25;
 		r.color = {125, 125, 125, 255};
 		world.entity().set<druid::raylib::Drawable>(r);
 	}
 
-	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::make_arc(640, 360, 100, -45, 300, 4, 120, {255, 255, 0, 255}));
-	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::make_arc(640, 360, 250, -50, 305, 4, 120, {255, 255, 0, 255}));
+	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::from_arc(
+		{.x = 640, .y = 360, .radius = 100, .angle_start = -45, .angle_end = 300, .line_width = 4, .segments = 120, .color = {255, 255, 0, 255}}));
+
+	world.entity().set<druid::raylib::Drawable>(druid::raylib::TriangleStrip::from_arc(
+		{.x = 640, .y = 360, .radius = 250, .angle_start = -50, .angle_end = 300, .line_width = 4, .segments = 305, .color = {255, 255, 0, 255}}));
 
 	// Run indefinitely.
 	while (world.progress())
 	{
 	}
+
+	// NOLINTEND
 
 	return EXIT_SUCCESS;
 }
