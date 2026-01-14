40d8c956ec1851997ccf18249cd9d2f6a51c578a
Jamilly_x86 <brownj.dupreerandom@gmail.com>
2026-01-14 16:36:30 -0500

    adding more test for move class

--- app/runestone/test/Move.test.cpp
+++ app/runestone/test/Move.test.cpp
@@ -13,9 +13,23 @@ TEST(Move, constructor)
 	EXPECT_EQ(move.raw(), 0);
 }
 
-// TEST(Move, basicRoundTrip)
-// {
-// 	constexpr auto move = runestone::Move::make({runestone::ChessBoard::Square::E2, runestone::ChessBoard::Square::E4}, runestone::Move::Type::Normal, runestone::Move::Promo::Queen);
-//
-// 	EXPECT_EQ()
-// }
+TEST(Move, basicRoundTrip)
+{
+	constexpr auto move = runestone::Move::make({.from = runestone::ChessBoard::Square::E2, .to = runestone::ChessBoard::Square::E4},
+												runestone::Move::Type::Normal, runestone::Move::Promo::Queen);
+
+	EXPECT_EQ(move.from(), runestone::ChessBoard::Square::E2);
+	EXPECT_EQ(move.to(), runestone::ChessBoard::Square::E4);
+	EXPECT_EQ(move.type(), runestone::Move::Type::Normal);
+}
+
+TEST(Move, promotionEncoding)
+{
+	constexpr auto move = runestone::Move::make({.from = runestone::ChessBoard::Square::A7, .to = runestone::ChessBoard::Square::A8},
+												runestone::Move::Type::Promotion, runestone::Move::Promo::Queen);
+
+	EXPECT_EQ(move.from(), runestone::ChessBoard::Square::A7);
+	EXPECT_EQ(move.to(), runestone::ChessBoard::Square::A8);
+	EXPECT_EQ(move.type(), runestone::Move::Type::Promotion);
+	EXPECT_EQ(move.promo(), runestone::Move::Promo::Queen);
+}
