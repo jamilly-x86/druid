3b98e0232eaa6f54060377b129fc7ad4bd752b33
Jamilly_x86 <brownj.dupreerandom@gmail.com>
2026-01-15 10:22:17 -0500

    adding tests for move.ixx

--- app/runestone/test/Move.test.cpp
+++ app/runestone/test/Move.test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <glm/fwd.hpp>
+#include <magic_enum/magic_enum.hpp>
 
 import runestone.move;
 import runestone.chessboard;
@@ -33,3 +35,65 @@ TEST(Move, promotionEncoding)
 	EXPECT_EQ(move.type(), runestone::Move::Type::Promotion);
 	EXPECT_EQ(move.promo(), runestone::Move::Promo::Queen);
 }
+
+TEST(Move, specialTypes)
+{
+	constexpr auto ep = runestone::Move::make({.from = runestone::ChessBoard::Square::E5, .to = runestone::ChessBoard::Square::D6},
+											  runestone::Move::Type::EnPassant, runestone::Move::Promo::Knight);
+	EXPECT_EQ(ep.type(), runestone::Move::Type::EnPassant);
+
+	constexpr auto castle = runestone::Move::make({.from = runestone::ChessBoard::Square::E1, .to = runestone::ChessBoard::Square::G1},
+											  runestone::Move::Type::Castling, runestone::Move::Promo::Knight);
+	EXPECT_EQ(castle.type(), runestone::Move::Type::Castling);
+}
+
+
+TEST(Move, rawBitLayout)
+{
+	constexpr auto move = runestone::Move::make({ .from = runestone::ChessBoard::Square::E2, .to = runestone::ChessBoard::Square::E4} ,
+										  runestone::Move::Type::Normal, runestone::Move::Promo::Queen);
+
+	constexpr auto raw = move.raw();
+
+	static constexpr std::uint16_t square_mask{0x3F};
+	static constexpr std::uint16_t two_bit_mask{0x3};
+	static constexpr auto from_shift{6};
+	static constexpr auto promo_shift{12};
+	static constexpr auto type_shift{14};
+
+	// Move.To
+	EXPECT_EQ(raw & square_mask, static_cast<glm::u16>(runestone::ChessBoard::Square::E4));
+	// Move.From
+	EXPECT_EQ(raw >> from_shift & square_mask, static_cast<glm::u16>(runestone::ChessBoard::Square::E2));
+	// Move.Promo
+	EXPECT_EQ(raw >> promo_shift & two_bit_mask, static_cast<glm::u16>(runestone::Move::Promo::Queen));
+	// Move.Type
+	EXPECT_EQ(raw >> type_shift & two_bit_mask, static_cast<glm::u16>(runestone::Move::Type::Normal));
+}
+
+// NOLINTBEGIN (readability-function-cognitive-complexity,-warnings-as-errors)
+TEST(Move, exhaustiveFromToRoundTrip)
+{
+	for (auto from : magic_enum::enum_values<runestone::ChessBoard::Square>())
+	{
+		if (from == runestone::ChessBoard::Square::Size)
+		{
+			continue;
+		}
+
+		for (auto to : magic_enum::enum_values<runestone::ChessBoard::Square>())
+		{
+			if (to == runestone::ChessBoard::Square::Size)
+			{
+				continue;
+			}
+
+			const auto move = runestone::Move::make({.from = from, .to = to}, runestone::Move::Type::Normal, runestone::Move::Promo::Queen);
+
+			EXPECT_EQ(move.from(), from);
+			EXPECT_EQ(move.to(), to);
+			EXPECT_EQ(move.type(), runestone::Move::Type::Normal);
+		}
+	}
+}
+// NOLINTEND
